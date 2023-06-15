e238766fd19a814953f1eb6484e62a69beb84824
shintaniyuta <cada3986@yahoo.co.jp>
2023-06-15 11:31:38 +0900

    aozora削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
-import '../stylesheets/application';
+import '../stylesheets/application.scss';
 import '../stylesheets/sample';
 
 Rails.start()
